d0eefd1389e3793d6ca8fdf0f43a1b569521f431
furioiusnur <furioiusnur@gmail.com>
2022-04-27 10:24:54 +0600

    All Friends and Hook component add

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
       {{name}}
+      <NavBar></NavBar>
+      <AllFriends :friends="friends" @delete="deleteFriend"></AllFriends>
+      <OnlineFriends :friends="friends"></OnlineFriends>
+      <HookSystem></HookSystem>
   </div>
 </template>
 
 <script>
-
+import NavBar from "@/components/NavBar";
+import AllFriends from "@/components/AllFriends";
+import OnlineFriends from "@/components/OnlineFriends";
+import HookSystem from "@/components/HookSystem";
 export default {
   name: 'App',
   data(){
     return{
-      name: 'VueJS Course'
+      name: 'VueJS Course',
+      friends:[
+        {name:'A', online:true},
+        {name:'B', online:false},
+        {name:'C', online:true},
+        {name:'D', online:true},
+        {name:'E', online:false},
+      ]
+    }
+  },
+  components:{
+    HookSystem,
+    NavBar,
+    AllFriends,
+    OnlineFriends
+  },
+  methods:{
+    deleteFriend(friendName){
+      // console.log(payload);
+      this.friends = this.friends.filter(friend=>{
+        return friend.name !== friendName;
+      })
     }
   }
 }
---
+++ vue/src/components/HookSystem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <button @click="changeHook">Change Hook</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HookSystem",
+  data() {
+    return {
+      title: 'Hook Part'
+    }
+  },
+  methods:{
+    changeHook(){
+      this.title = 'Hook Update Part'
+    }
+  },
+  beforeCreate() {
+    alert('before create');
+  },
+  created() {
+    alert('created');
+  },
+  beforeUpdate() {
+    alert('Do you really wants to update Hook name?');
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
