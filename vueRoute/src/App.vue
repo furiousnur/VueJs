45d9b7e5cb3429aa3092f7824557d20c25d4d7f2
furioiusnur <furioiusnur@gmail.com>
2022-04-27 15:58:49 +0600

    navbar added and router link created

--- vueRoute/src/App.vue
+++ vueRoute/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <NavBar></NavBar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar";
+
 export default {
   name: 'App',
+  components:{
+    NavBar
+  }
 }
 </script>
 
